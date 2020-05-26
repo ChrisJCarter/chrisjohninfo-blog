43eebe01dfb5cd8660f83f7d57f029b91d98983b
chrcar01 <chrisjcarter@gmail.com>
2020-05-25 17:49:55 -0700

    post preview

--- src/ChrisJohnInfo.Blog.Contracts/Interfaces/IBlogService.cs
+++ src/ChrisJohnInfo.Blog.Contracts/Interfaces/IBlogService.cs
@@ -1,4 +1,5 @@
 ﻿using ChrisJohnInfo.Blog.Contracts.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,5 +8,6 @@ namespace ChrisJohnInfo.Blog.Contracts.Interfaces
     public interface IBlogService
     {
         Task<IEnumerable<PostViewModel>> GetPosts();
+        Task<PostViewModel> GetPost(Guid postId);
     }
 }
