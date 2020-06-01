39fae542e35445220b3157540880e0a7bb4065cb
chrcar01 <chrisjcarter@gmail.com>
2020-05-31 21:22:37 -0700

    moved razor builder() back to method

--- src/ChrisJohnInfo.Blog.Core/Transformers/RazorTransformer.cs
+++ src/ChrisJohnInfo.Blog.Core/Transformers/RazorTransformer.cs
@@ -1,25 +1,19 @@
 ﻿using ChrisJohnInfo.Blog.Contracts.Interfaces;
+using RazorLight;
 using System;
 using System.Threading.Tasks;
-using RazorLight;
 
 namespace ChrisJohnInfo.Blog.Core.Transformers
 {
     public class RazorTransformer : IContentTransformer
     {
-        private readonly RazorLightEngine _razor;
-
-        public RazorTransformer()
-        {
-            _razor = new RazorLightEngineBuilder()
-                .UseEmbeddedResourcesProject(typeof(RazorTransformer))
-                .Build();
-        }
-
         public async Task<string> TransformAsync(Guid postId, string content)
         {
+            var razor = new RazorLightEngineBuilder()
+                .UseEmbeddedResourcesProject(typeof(RazorTransformer))
+                .Build();
             var razorTemplateHelper = new RazorTemplateHelper {Host = "", PostId = postId};
-            return await _razor.CompileRenderStringAsync(postId.ToString(), content, razorTemplateHelper);
+            return await razor.CompileRenderStringAsync(postId.ToString(), content, razorTemplateHelper);
         }
     }
 }
